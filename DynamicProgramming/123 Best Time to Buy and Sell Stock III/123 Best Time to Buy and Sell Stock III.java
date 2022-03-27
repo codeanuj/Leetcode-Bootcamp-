e42c220c862ae2927c1fac7eb0e50e3f58628c8f
Anuj <80551063+codeanuj@users.noreply.github.com>
2022-03-27 15:41:09 +0530

    Create 123 Best Time to Buy and Sell Stock III.java

---
+++ DynamicProgramming/123 Best Time to Buy and Sell Stock III/123 Best Time to Buy and Sell Stock III.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int dp[][][] = new int[prices.length][2][3];
+        
+        for(int i=0; i<prices.length; i++)
+            for(int j=0; j<2; j++)
+                for(int k=0; k<3; k++)
+                    dp[i][j][k] = -1;
+        /*Two transaction allowed atmost*/
+        return getMaxProfitWithTwoTransaction(0, prices, 1, 2, dp);
+    }
+    
+    private int getMaxProfitWithTwoTransaction(int curr_day, int[] prices, int canBuy, int transCount, int[][][] dp){
+        
+        if(curr_day>=prices.length || transCount==0) return 0;
+        
+        if(dp[curr_day][canBuy][transCount] != -1) return dp[curr_day][canBuy][transCount];
+        
+        int ans=0;
+        
+        int idle = getMaxProfitWithTwoTransaction(curr_day+1, prices, canBuy, transCount, dp);
+        
+        if(canBuy==1){
+            int buy = -prices[curr_day]+ getMaxProfitWithTwoTransaction(curr_day+1, prices, 0, transCount, dp);
+            ans=Math.max(idle, buy);
+        }else{
+            int sell = prices[curr_day]+ getMaxProfitWithTwoTransaction(curr_day+1, prices, 1, transCount-1, dp);
+            ans= Math.max(idle, sell);
+        }
+        
+        dp[curr_day][canBuy][transCount]=ans;
+        return dp[curr_day][canBuy][transCount];
+    }       
+}
