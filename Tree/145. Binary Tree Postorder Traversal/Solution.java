91cf28d668a146148834cf5e8f03793e2e4d6702
Anuj <80551063+codeanuj@users.noreply.github.com>
2024-03-02 13:25:18 +0530

    Update Solution.java

--- Tree/145. Binary Tree Postorder Traversal/Solution.java
+++ Tree/145. Binary Tree Postorder Traversal/Solution.java
@@ -27,4 +27,50 @@ class Solution {
         list.add(root.val);
         return;
     }
+
+
+    //Iterative Solution
+
+    public List<Integer> postorderTraversalWithIteration(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        if(root == null) return list;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode curr_node = stack.pop();
+            list.add(curr_node.val);
+            if(curr_node.left!=null) stack.push(curr_node.left);
+            if(curr_node.right!=null) stack.push(curr_node.right);
+        }
+        Collections.reverse(list);
+        return list;
+    }
 }
+
+
+    public List<Integer> postorderTraversalWithoutReversing(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current_Node =root;
+        TreeNode temp=null;
+        while(current_Node != null || !stack.isEmpty()){
+            if(current_Node != null){
+                stack.push(current_Node);
+                current_Node = current_Node.left;
+            }else{
+                temp = stack.peek().right;
+                if(temp == null){
+                    temp = stack.pop();
+                    list.add(temp.val);
+                    while(!stack.isEmpty() && temp == stack.peek().right){
+                        temp = stack.pop();
+                        list.add(temp.val);
+                    }
+
+                }else{
+                    current_Node = temp;
+                }
+            }          
+        }
+        return list;
+    }
