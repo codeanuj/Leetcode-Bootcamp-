354232e8e9ea2e525713ad3cac03bc089850d0da
Anuj <80551063+codeanuj@users.noreply.github.com>
2024-03-03 19:54:59 +0530

    Update Solution.java

--- Tree/94. Binary Tree Inorder Traversal/Solution.java
+++ Tree/94. Binary Tree Inorder Traversal/Solution.java
@@ -27,6 +27,51 @@ class Solution {
         inorderTraversal(root.right, list);
         return;
     }
+
+
+    //Iterative Solution
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+       List<Integer> list = new ArrayList<Integer>();
+       Stack<TreeNode> stack = new Stack<>();
+       if(root == null) return list;
+
+       //Add Left Subtree of root node
+       addLeftSubTree(stack, root);
+
+       while(!stack.isEmpty()){
+           //If left child is null pop the top node and add it
+           TreeNode currentNode = stack.pop();
+           list.add(currentNode.val);
+           // if root right is not null add the leftsubtree of right child
+           if(currentNode.right != null) addLeftSubTree(stack, currentNode.right);
+        }
+       return list;
+    }
+
+    private void addLeftSubTree(Stack<TreeNode> stack, TreeNode currentNode){
+        stack.push(currentNode);
+        while(currentNode.left != null){
+            stack.push(currentNode.left);
+            currentNode = currentNode.left;
+        }
+    }
+}
 //Iterative Solution
       public List<Integer> inorderTraversal(TreeNode root) {
        List<Integer> list = new ArrayList<Integer>();
