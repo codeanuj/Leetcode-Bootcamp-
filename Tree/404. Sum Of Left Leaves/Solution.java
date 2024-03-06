a4dc9757cf6aa4ca041be3b960c5727befda5c16
Anuj <80551063+codeanuj@users.noreply.github.com>
2024-03-06 22:49:26 +0530

    Update Solution.java

--- Tree/404. Sum Of Left Leaves/Solution.java
+++ Tree/404. Sum Of Left Leaves/Solution.java
@@ -28,4 +28,33 @@ class Solution {
         int b = findSum(root.right, false);
         return a+b;
     }
+
+
+
+    //BY BFS
+
+     public int sumOfLeftLeaves(TreeNode root) {
+        int answer=0;
+        Queue<Pair> queue = new LinkedList<>();
+        queue.offer(new Pair(root, false));
+
+        while(!queue.isEmpty()){
+            Pair pair = queue.poll();
+            TreeNode node = pair.node;
+            boolean isLeft = pair.isLeft;
+            if(node.left == null && node.right==null && isLeft) answer+=node.val;
+            if(node.left!=null) queue.offer(new Pair(node.left, true));
+            if(node.right!=null) queue.offer(new Pair(node.right, false));
+        }      
+        return answer;    
+    }
+
+    class Pair{
+        TreeNode node;
+        boolean isLeft;
+
+        public Pair(TreeNode node, boolean isLeft){
+            this.node = node;
+            this.isLeft = isLeft;
+        }
 }
