a8e15809cdecce00ac4a38be1fcdc6245f7b9297
Anuj <80551063+codeanuj@users.noreply.github.com>
2024-05-15 07:53:55 +0200

    Update Solution.java

--- Recursion And Backtracking/17 Letter Combinations of a Phone Number/Solution.java
+++ Recursion And Backtracking/17 Letter Combinations of a Phone Number/Solution.java
@@ -1,9 +1,8 @@
 class Solution {
     List<String> answer = new ArrayList<>();
-    HashMap<Character, String> map = new HashMap<>();
+    String[] KEYS_MAPPING = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     public List<String> letterCombinations(String digits) {
         if(digits.length()==0) return answer;
-        createPhone();
         letterCombinations(digits, 0, new StringBuilder());
         return answer;        
     }
@@ -14,23 +13,12 @@ class Solution {
             return;
         }
         char number = digits.charAt(curr_index);
-        String alphabet = map.get(number);
+        String alphabet = KEYS_MAPPING[number - '0'];
         for(int i=0; i<alphabet.length(); i++){
             combination.append(alphabet.charAt(i));
             letterCombinations(digits, curr_index+1, combination);
             combination.deleteCharAt(combination.length()-1);
         }
         return;
-    }
-
-    private void createPhone(){
-        map.put('2', "abc");
-        map.put('3', "def");
-        map.put('4', "ghi");
-        map.put('5', "jkl");
-        map.put('6', "mno");
-        map.put('7', "pqrs");
-        map.put('8', "tuv");
-        map.put('9', "wxyz");
     }
 }
